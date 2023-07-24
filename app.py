cecbb4b8c6f80308809fab9045029dfcd4ad4754
EC2 Default User <ec2-user@ip-172-31-82-40.ec2.internal>
2023-07-24 09:56:26 +0000

    Added

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ BuildSpecStack(app, "BuildSpecStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    env=cdk.Environment(account='748389823986', region='us-east-1'),
+    env=cdk.Environment(account='079774713790', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
